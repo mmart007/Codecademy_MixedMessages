a96a779b581cf86ef11d558040082234dd339dcc
mmart007 <97489267+mmart007@users.noreply.github.com>
2022-01-14 20:33:21 -0500

    Final version

--- Mixed messages v2.js
+++ Mixed messages v2.js
@@ -3,9 +3,9 @@ const rNum = (aLen) => {
 }
 
 const medFeedback = {
-    greet: ['Hey there', 'Yo fool','What\'s up?', 'How you doin\'?'],
-    advice: ['Take this med', 'Go fly a kite', 'I\'m retiring', 'You\'re all good'],
-    follow: [1, 2, 3, 4, 5, 6]
+    greet: ['Hey there.', 'Yo fool.','What\'s up?', 'How you doin\'?'],
+    advice: ['take this med', 'go fly a kite', 'I\'m retiring', 'you\'re all good'],
+    follow: [2, 3, 4, 5, 6, 8]
 }
 
 let message = [];
@@ -15,16 +15,25 @@ for (let i in medFeedback){
         case 'greet':
             randItem = medFeedback[i][rNum(medFeedback[i].length)];
             message.push(randItem);
+            break;
             //console.log(`${randItem}`);
         case 'advice':
             randItem = medFeedback[i][rNum(medFeedback[i].length)];
             message.push(`Based on this visit ${randItem}.`);
-        case 'advice':
+            break;
+        case 'follow':
             randItem = medFeedback[i][rNum(medFeedback[i].length)];
             message.push(`See you in ${randItem} weeks.`);
+            break;
         default :
             message.push(`I need to finish`);
+            break;
     }
-    
 }
 
+formatMsg = (mInput) => {
+    const mOutput = message.join(' ');
+    console.log(mOutput);
+}
+
+formatMsg(message);
