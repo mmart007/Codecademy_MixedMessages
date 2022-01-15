6e75d141619fb68cd12585d35681a4559be85895
mmart007 <97489267+mmart007@users.noreply.github.com>
2022-01-14 20:37:10 -0500

    Final version

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -4,44 +4,42 @@ const fullMsg = {
     const rnum = Math.floor(Math.random()*4)
     switch (rnum){
        case 0:
-           console.log('Yo!')
+           return 'Yo!'
            break;
        case 1:
-           console.log('Hola!');
+           return 'Hola!';
            break;
        case 2:
-           console.log('Cheese man in the house')
+           return 'Cheese man in the house';
            break;
        case 3:
-           console.log('Allo.');
+           return 'Allo.';
            break;
        default:
-           console.log('Check your random!')
+           return 'Check your random!';
            break;
     }
 },
 
  apt(userNum = Math.floor(Math.random()*24)){
-    console.log(`Your appointment is at ${userNum}:00`)
+    return `Your appointment is at ${userNum}:00`
 },
 
  meds(dx = "Schizophrenia") {
     switch (dx){
         case 'Schizophrenia':
-            console.log('Antipsychotics');
+            return 'Antipsychotics';
             break;
         case 'Depression':
-            console.log('Happy pills');
+            return 'Happy pills';
             break;
         case 'Anxiety':
-            console.log('Meditation');
+            return 'Meditation';
             break;
         default:
-            console.log('I\'m not sure I can help you if you can\'t tell me a real diagnosis');
+            return 'I\'m not sure I can help you if you can\'t tell me a real diagnosis';
     }
 }
 }
 
-console.log(fullMsg.userName);
-fullMsg.greeting();
-fullMsg.meds();
+console.log(`${fullMsg.greeting()} ${fullMsg.userName}. ${fullMsg.meds('Schizophrenia')} are recommended. ${fullMsg.apt()} `)
